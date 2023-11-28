8b6ae4a0e6664017fd2d87d451a4d9e15072f45d
Andrew Hudzenko PC <s1mpleworkpost@gmail.com>
2023-11-28 19:46:03 +0200

    add discover-sever (eureka)

---
+++ discovery-server/src/test/java/com/andersen/discoveryservice/DiscoveryServiceApplicationTest.java
@@ -0,0 +1,12 @@
+package com.andersen.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTest {
+    @Test
+    void contextLoads() {
+
+    }
+}
