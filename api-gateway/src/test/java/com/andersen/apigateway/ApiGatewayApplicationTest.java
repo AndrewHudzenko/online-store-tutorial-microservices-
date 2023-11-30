8b63bf61dfc346fc0f3e85108bed8983253d1771
Andrew Hudzenko <s1mpleworkpost@gmail.com>
2023-11-30 12:52:22 +0200

    add api-gateway service

---
+++ api-gateway/src/test/java/com/andersen/apigateway/ApiGatewayApplicationTest.java
@@ -0,0 +1,12 @@
+package com.andersen.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTest {
+    @Test
+    void contextLoads() {
+
+    }
+}
